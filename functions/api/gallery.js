9bf8cf0f29e38c749306b086ead13a0bd51173fd
wcz1459 <101039439+wcz1459@users.noreply.github.com>
2025-06-21 11:09:36 +0800

    Add Photos

--- functions/api/gallery.js
+++ functions/api/gallery.js
@@ -1,6 +1,12 @@
 // functions/api/gallery.js
 const images = [
-    { src: '/assets/images/photo1.jpg', width: 1952, height: 1280, alt: '我们的毕业照' },
+    { src: '/assets/images/photo0.jpg', width: 1952, height: 1280, alt: '我们的毕业照' },
+    { src: '/assets/images/Photo1.jpg', width: 1706, height: 1280, alt: '长卷' },
+    { src: '/assets/images/Photo2.jpg', width: 1707, height: 1280, alt: '某张小照' },
+    { src: '/assets/images/Photo3.jpg', width: 1706, height: 1279, alt: '聚会1' },
+    { src: '/assets/images/Photo4.jpg', width: 1706, height: 1279, alt: '画廊' },
+    { src: '/assets/images/Photo5.jpg', width: 1706, height: 1279, alt: '合唱' },
+    { src: '/assets/images/Photo0476.jpg', width: 1706, height: 1280, alt: '合唱前' },
     // 在这里添加更多图片信息...
 ];
 
